bea162e1a7abe952a237a5e687a330e24c606e11
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2018-10-28 12:19:57 -0700

    added expo three.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import Expo from "expo";
+import ExpoTHREE, { THREE } from "expo-three";
 
 export default class HomeScreen extends React.Component {
-   
+  static navigationOptions = {
+    header: null
+  };
+
+  onContextCreate = async ({ gl, scale, width, height, arSession }) => {};
+
+  onRender = delta => {};
 
   render() {
-    return (
-      <View style={{paddingTop:50}}>
-        <Text>Home Screen</Text>
-      </View>
-    );
+    return <ExpoGraphics.View style={{ flex: 1 }} 
+      onContextCreate={this.onContextCreate}
+      onRender={this.onRender}
+      arEnabled={true}
+    />;
   }
 }

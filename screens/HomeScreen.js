d8a7da145ccae8a2aafd444536dca19b189ef711
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2018-10-28 12:58:39 -0700

    added camera

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,48 @@
 import React from "react";
-import { Text, View } from "react-native";
-import Expo from "expo";
 import ExpoTHREE, { THREE } from "expo-three";
+
+console.disableYellowBox = true;
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
 
-  onContextCreate = async ({ gl, scale, width, height, arSession }) => {};
+  onContextCreate = async ({ gl, scale, width, height, arSession }) => {
+    //initialize renderer
+    this.renderer = ExpoTHREE.createRenderer({ gl });
+    this.renderer = setPixelRatio(scale);
+    this.renderer.setSize(width, height);
 
-  onRender = delta => {};
+    //initialize scene
+    this.scene = new THREE.Scene();
+    this.scene.background = ExpoTHREE.createARBackgroundTexture(
+      arSession,
+      this.renderer
+    );
+
+    //initialize camera
+    this.camera = ExpoTHREE.createARCamera(
+      arSession,
+      width / scale,
+      height / scale,
+      0.01,
+      1000
+    );
+  };
+
+  onRender = delta => {
+    this.renderer.render(this.scene, this.camera);
+  };
 
   render() {
-    return <ExpoGraphics.View style={{ flex: 1 }} 
-      onContextCreate={this.onContextCreate}
-      onRender={this.onRender}
-      arEnabled={true}
-    />;
+    return (
+      <ExpoGraphics.View
+        style={{ flex: 1 }}
+        onContextCreate={this.onContextCreate}
+        onRender={this.onRender}
+        arEnabled={true}
+      />
+    );
   }
 }
